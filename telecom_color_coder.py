b6a88ba3dda5b9f072728e08c1d08049e5306f08
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2024-07-15 19:57:36 +0530

    Update telecom_color_coder.py

--- telecom_color_coder.py
+++ telecom_color_coder.py
@@ -1,30 +1,11 @@
+class TelecomColorCode:
+    MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+    MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
-MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    def __init__(self, major_index, minor_index):
+        self.major_color = self.MAJOR_COLORS[major_index]
+        self.minor_color = self.MINOR_COLORS[minor_index]
 
-
-def color_pair_to_string(major_color, minor_color):
-  return f'{major_color} {minor_color}'
-
-
-def get_color_from_pair_number(pair_number):
-  zero_based_pair_number = pair_number - 1
-  major_index = zero_based_pair_number // len(MINOR_COLORS)
-  if major_index >= len(MAJOR_COLORS):
-    raise Exception('Major index out of range')
-  minor_index = zero_based_pair_number % len(MINOR_COLORS)
-  if minor_index >= len(MINOR_COLORS):
-    raise Exception('Minor index out of range')
-  return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
-
-
-def get_pair_number_from_color(major_color, minor_color):
-  try:
-    major_index = MAJOR_COLORS.index(major_color)
-  except ValueError:
-    raise Exception('Major index out of range')
-  try:
-    minor_index = MINOR_COLORS.index(minor_color)
-  except ValueError:
-    raise Exception('Minor index out of range')
-  return major_index * len(MINOR_COLORS) + minor_index + 1
+    def __str__(self):
+        # Return a string representation of the color code
+        return f'{self.major_color} {self.minor_color}'
