9fa6e8252d7d9a0d01363519a52afa4ae910d14c
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2024-07-15 20:19:03 +0530

    Update get_color_from_number.py

--- get_color_from_number.py
+++ get_color_from_number.py
@@ -15,6 +15,16 @@ def validate_minor_index(minor_index):
         raise IndexError('Minor index out of range')
 
 def get_color_from_pair_number(pair_number):
+     """
+    Returns the TelecomColorCode based on the given pair number.
+    Args:
+        pair_number (int): The pair number for which the color code needs to be determined.
+    Returns:
+        TelecomColorCode: The color code corresponding to the given pair number.
+    Raises:
+        ValueError: If the major or minor index is invalid.
+
+    """
     major_index = calculate_major_index(pair_number)
     validate_major_index(major_index)
     minor_index = calculate_minor_index(pair_number)
