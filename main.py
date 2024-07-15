c95176a6f38cc3065a3bfa583db476f6569dac85
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2024-07-15 20:16:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from color_reference_manual import print_reference_manual
+from get_numbers_from_color import get_pair_number_from_color
+
+if __name__ == "__main__":
+    print_reference_manual()
+    print(get_pair_number_from_color("Red","Blue"))
+    print(get_pair_number_from_color("White","Blue"))
